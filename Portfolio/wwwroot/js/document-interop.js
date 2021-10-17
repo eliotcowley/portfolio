f2d7c7fd72aa5883a32d0a4c6b8ee5716a33199c
Eliot Cowley <eliot93@live.com>
2021-10-16 21:46:53 -0700

    Added back to top button

--- Portfolio/wwwroot/js/document-interop.js
+++ Portfolio/wwwroot/js/document-interop.js
@@ -2,3 +2,26 @@
 portfolio.setDocumentTitle = function (title) {
     document.title = title;
 };
+
+var backToTopButton = document.getElementById("btn-back-to-top");
+var backToTopButtonScrollAmount = 300;
+
+window.onscroll = function () {
+    if (backToTopButton == null) {
+        backToTopButton = document.getElementById("btn-back-to-top");
+    }
+
+    console.log(document.body.scrollTop);
+    console.log(document.documentElement.scrollTop);
+
+    if (document.documentElement.scrollTop > backToTopButtonScrollAmount) {
+        backToTopButton.style.display = "block";
+    }
+    else {
+        backToTopButton.style.display = "none";
+    }
+}
+
+function OnScrollEvent() {
+    document.documentElement.scrollTop = 0;
+}

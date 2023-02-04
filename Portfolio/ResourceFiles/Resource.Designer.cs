d1f689b0c8ef298546762ae00dd0d2d9a4b7a158
Eliot Cowley <eliot93@live.com>
2023-02-04 21:23:05 +0900

    Updated jlpt to n1

--- Portfolio/ResourceFiles/Resource.Designer.cs
+++ Portfolio/ResourceFiles/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace Portfolio.ResourceFiles {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -187,7 +187,7 @@ namespace Portfolio.ResourceFiles {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Japanese (JLPT N3).
+        ///   Looks up a localized string similar to Japanese (JLPT N1).
         /// </summary>
         public static string Japanese {
             get {
